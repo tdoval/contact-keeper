28f00242403ca3a77631aaeaaacaad78366dce37
Tdoval <thiagodovalaraujo@gmail.com>
2021-09-06 20:11:19 -0300

    form add contact

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,19 +1,34 @@
 import React, { Fragment, useContext } from 'react';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import ContactItem from './ContactItem';
 import ContactContext from '../../context/contact/ContactContext';
 
 export const Contacts = () => {
-    const contactContext = useContext(ContactContext);
+  const contactContext = useContext(ContactContext);
 
-    const { contacts } = contactContext;
+  const { contacts, filtered } = contactContext;
 
-    return (
-        <Fragment>
-            {contacts.map((contact) => (
-                <ContactItem key={contact.id} contact={contact} />
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
+
+  return (
+    <Fragment>
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact._id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact._id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
             ))}
-        </Fragment>
-    );
+      </TransitionGroup>
+    </Fragment>
+  );
 };
 
 export default Contacts;
--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -37,13 +37,15 @@ const ContactState = (props) => {
         type: 'personal',
       },
       {
-        id: 3,
+        id: 4,
         name: 'Harry White',
         email: 'whiteharry_@gmail.com',
         phone: '444-231-1234',
         type: 'professional',
       },
     ],
+    current: null,
+    filtered: null,
   };
 
   const [state, dispatch] = useReducer(ContactReducer, initialState);
@@ -60,22 +62,46 @@ const ContactState = (props) => {
   };
 
   // Delete Contact
+  const deleteContact = (id) => {
+    dispatch({ type: DELETE_CONTACT, payload: id });
+  };
 
   //Set Current Contact
+  const setCurrent = (contact) => {
+    dispatch({ type: SET_CURRENT, payload: contact });
+  };
 
   // Clear Current Contact
+  const clearCurrent = (contact) => {
+    dispatch({ type: CLEAR_CURRENT });
+  };
 
   // Update Contact
-
+  const updateContact = (contact) => {
+    dispatch({ type: UPDATE_CONTACT, payload: contact });
+  };
   // Filter Contacts
+  const filterContacts = (text) => {
+    dispatch({ type: FILTER_CONTACTS, payload: text });
+  };
 
-  // Clera Filter
-
+  // Clear Filter
+  const clearFilter = (contact) => {
+    dispatch({ type: CLEAR_FILTER });
+  };
   return (
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        current: state.current,
+        filtered: state.filtered,
         addContact,
+        deleteContact,
+        setCurrent,
+        clearCurrent,
+        updateContact,
+        filterContacts,
+        clearFilter,
       }}
     >
       {props.children}
