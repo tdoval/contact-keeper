bc26ea041d0d2b767fdd052b359b5f5d307aa32a
tdoval <thiagodovalaraujo@gmail.com>
2022-09-11 15:40:31 -0300

    Test Add Contact

--- client/cypress/e2e/addContact.cy.js
+++ client/cypress/e2e/addContact.cy.js
@@ -1,3 +1,5 @@
+const randomName = require('random-name');
+
 describe('Add Contact', () => {
   it('User can login and Add new Contact', () => {
     //  login
@@ -7,5 +9,17 @@ describe('Add Contact', () => {
     cy.findByRole('button', { name: /login/i }).click();
 
     // Add Contact
+    const firstName = randomName.first();
+    const lastName = randomName.last();
+    cy.findByRole('textbox', { name: /name/i }).type(
+      firstName + ' ' + lastName
+    );
+    cy.findByRole('textbox', { name: /email/i }).type(
+      firstName + '_' + lastName + '@test.com'
+    );
+    const phoneNumber = Math.floor(100000000 + Math.random() * 900000000);
+    cy.findByRole('textbox', { name: /phone/i }).type(phoneNumber);
+    cy.findByRole('radio', { name: /professional/i }).check();
+    cy.findByRole('button', { name: /add contact/i }).click();
   });
 });
